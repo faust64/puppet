325a193b3aece403fd691e4d003d9570bb4f85db
Samuel Martin Moro <samuel@unetresgrossebite.com>
2017-05-31 16:09:50 +0200

    ensure we would not pull bacula from backports
    as I've had that problem a couple times, ... everything looks good,
    until the backup job fails, when client version doesn't match
    servers

--- modules/bacula/manifests/debian.pp
+++ modules/bacula/manifests/debian.pp
@@ -11,6 +11,11 @@ class bacula::debian {
 
 	Common::Define::Package["bacula-fd"]
 	    -> Common::Define::Service["bacula-fd"]
+
+	if (defined(Apt::Define::Repo["backports"])) {
+	    Common::Define::Package["bacula-fd"]
+		-> Apt::Define::Repo["backports"]
+	}
     }
 
     if ($bacula::vars::director_host == $fqdn) {
@@ -25,6 +30,13 @@ class bacula::debian {
 	Common::Define::Package["bacula-director-mysql"]
 	    -> Mysql::Define::Create_database["bacula"]
 	    -> Common::Define::Service["bacula-director"]
+
+	if (defined(Apt::Define::Repo["backports"])) {
+	    Common::Define::Package["bacula-common-mysql"]
+		-> Apt::Define::Repo["backports"]
+	    Common::Define::Package["bacula-director"]
+		-> Apt::Define::Repo["backports"]
+	}
     }
 
     if ($bacula::vars::storage_host == $fqdn) {
@@ -34,6 +46,11 @@ class bacula::debian {
 
 	Common::Define::Package["bacula-sd"]
 	    -> Common::Define::Service["bacula-sd"]
+
+	if (defined(Apt::Define::Repo["backports"])) {
+	    Common::Define::Package["bacula-sd"]
+		-> Apt::Define::Repo["backports"]
+	}
     }
 
     Package["bacula-common"]
