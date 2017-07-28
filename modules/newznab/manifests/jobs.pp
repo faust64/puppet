3cc3019c184a25b5ff7aae36c281e402126d2b6b
Samuel Martin Moro <samuel@unetresgrossebite.com>
2017-07-28 02:07:24 +0200

    lately, I've got warning of jobs not running for lockfile existing
    in all fairness, as database grew, I had to move out of these
    SSD-based kimsufi, ... too bad they don't have bigger SSDs, MySQL
    already misses it ...

--- modules/newznab/manifests/jobs.pp
+++ modules/newznab/manifests/jobs.pp
@@ -2,7 +2,7 @@ class newznab::jobs {
     cron {
 	"Update newznab indexes":
 	    command => "/usr/local/sbin/newznab_update >/dev/null 2>&1",
-	    hour    => "*",
+	    hour    => "*/2",
 	    minute  => 26,
 	    require => File["Install newznab update script"],
 	    user    => root;
