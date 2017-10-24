63ee33454147c426726b964fcc83ff8c26b1760d
Samuel Martin Moro <samuel@unetresgrossebite.com>
2017-10-24 03:50:16 +0200

    fix(apache): stretch support / ssl specifics

--- modules/apache/manifests/vars.pp
+++ modules/apache/manifests/vars.pp
@@ -84,7 +84,7 @@ class apache::vars {
     $with_collectd        = lookup("apache_collectd")
     $web_root             = lookup("apache_web_root")
 
-    if ($lsbdistcodename == "stretch" or $lsbdistcodename == "jessie" or $lsbdistcodename == "trusty") {
+    if ($lsbdistcodename == "stretch" or $lsbdistcodename == "jessie" or $lsbdistcodename == "trusty" or $lsbdistcodename == "xenial") {
 	$version = "2.4"
     } else {
 	$version = "2.2"
