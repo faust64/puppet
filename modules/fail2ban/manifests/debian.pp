1da8ef795024d256d20e807b3b65d8641feff3a1
Samuel Martin Moro <samuel@unetresgrossebite.com>
2017-10-24 04:24:11 +0200

    fix(fail2ban): stretch support

--- modules/fail2ban/manifests/debian.pp
+++ modules/fail2ban/manifests/debian.pp
@@ -1,4 +1,6 @@
 class fail2ban::debian {
+    $conf_dir = $fail2ban::vars::conf_dir
+
     common::define::package {
 	"fail2ban":
     }
@@ -11,6 +13,11 @@ class fail2ban::debian {
 	    owner   => root,
 	    path    => "/etc/default/fail2ban",
 	    source  => "puppet:///modules/fail2ban/defaults",
+	    require => Package["fail2ban"];
+	"Drop debian defaults":
+	    ensure  => absent,
+	    notify  => Service["fail2ban"],
+	    path    => "$conf_dir/jail.d/defaults-debian.conf",
 	    require => Package["fail2ban"];
     }
 
