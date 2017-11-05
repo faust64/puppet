ead6f74624dd97a913b34d98f9659dbe03ef4196
Samuel Martin Moro <samuel@unetresgrossebite.com>
2017-11-05 15:23:05 +0100

    fix(squid): prepare for stretch

--- modules/squid/manifests/debian.pp
+++ modules/squid/manifests/debian.pp
@@ -1,12 +1,21 @@
 class squid::debian {
-    common::define::package {
-	"squid3":
+    if ($lsbdistcodename == "stretch") {
+	common::define::package {
+	    "squid":
+	}
+
+	Package["squid"]
+	    -> File["Prepare Squid for further configuration"]
+    } else {
+	common::define::package {
+	    "squid3":
+	}
+
+	Package["squid3"]
+	    -> File["Prepare Squid for further configuration"]
     }
 
     if ($squid::vars::apt_cacher != false) {
 	include jesred
     }
-
-    Package["squid3"]
-	-> File["Prepare Squid for further configuration"]
 }
