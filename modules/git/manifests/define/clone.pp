679542e48fc36f58eb22fb736f3a8d5870cf551b
Samuel Martin Moro <samuel@unetresgrossebite.com>
2017-11-17 15:54:33 +0100

    feat(github): submodule init hook

--- modules/git/manifests/define/clone.pp
+++ modules/git/manifests/define/clone.pp
@@ -4,6 +4,7 @@ define git::define::clone($branch          = "master",
 			  $local_container = false,
 			  $local_name      = $name,
 			  $repository      = false,
+			  $submoduleinit   = false,
 			  $update          = false) {
     if ($repository and $local_container) {
 	if (! defined(Class[git])) {
@@ -15,23 +16,57 @@ define git::define::clone($branch          = "master",
 	    $realrepo = $repository
 	}
 
-	exec {
-	    "GIT clone $name":
-		command => "git clone -b $branch $realrepo $local_name",
-		creates => "$local_container/$local_name",
-		cwd     => $local_container,
-		path    => "/usr/local/bin:/usr/bin:/bin",
-		require => Package["git"];
+	if ($submoduleinit) {
+	    exec {
+		"GIT clone $name":
+		    command => "git clone -b $branch $realrepo $local_name",
+		    creates => "$local_container/$local_name",
+		    cwd     => $local_container,
+		    notify  => Exec["GIT init submodule $name"],
+		    path    => "/usr/local/bin:/usr/bin:/bin",
+		    require => Package["git"];
+	    }
+	} else {
+	    exec {
+		"GIT clone $name":
+		    command => "git clone -b $branch $realrepo $local_name",
+		    creates => "$local_container/$local_name",
+		    cwd     => $local_container,
+		    path    => "/usr/local/bin:/usr/bin:/bin",
+		    require => Package["git"];
+	    }
 	}
 
 	if ($update) {
+	    if ($submoduleinit) {
+		exec {
+		    "GIT update $name":
+			command => "git pull",
+			cwd     => "$local_container/$local_name",
+			notify  => Exec["GIT init submodule $name"],
+			unless  => "git fetch -v --dry-run 2>&1 | grep $branch | grep 'up to date'",
+			path    => "/usr/local/bin:/usr/bin:/bin",
+			require => Exec["GIT clone $name"];
+		}
+	    } else {
+		exec {
+		    "GIT update $name":
+			command => "git pull",
+			cwd     => "$local_container/$local_name",
+			unless  => "git fetch -v --dry-run 2>&1 | grep $branch | grep 'up to date'",
+			path    => "/usr/local/bin:/usr/bin:/bin",
+			require => Exec["GIT clone $name"];
+		}
+	    }
+	}
+	if ($submoduleinit) {
 	    exec {
-		"GIT update $name":
-		    command => "git pull",
-		    cwd     => "$local_container/$local_name",
-		    unless  => "git fetch -v --dry-run 2>&1 | grep $branch | grep 'up to date'",
-		    path    => "/usr/local/bin:/usr/bin:/bin",
-		    require => Exec["GIT clone $name"];
+		"GIT init submodule $name":
+		    command     => "git submodule update --init",
+		    cwd         => "$local_container/$local_name",
+		    path        => "/usr/local/bin:/usr/bin:/bin",
+		    refreshonly => true,
+		    require     => Exec["GIT clone $name"];
 	    }
 	}
     } elsif ($repository) {
