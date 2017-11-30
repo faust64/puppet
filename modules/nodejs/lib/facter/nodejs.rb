afc3186df73fd8927225cebe74414b18376f3f45
Samuel Martin Moro <samuel@unetresgrossebite.com>
2017-12-01 00:49:08 +0100

    fix(nodejs): divert stderr

--- modules/nodejs/lib/facter/nodejs.rb
+++ modules/nodejs/lib/facter/nodejs.rb
@@ -1,15 +1,15 @@
 Facter.add("nodeversion") do
     setcode do
-	%x{node --version | sed 's|^v||'}.chomp
+	%x{node --version 2>/dev/null | sed 's|^v||'}.chomp
     end
 end
 Facter.add("npmversion") do
     setcode do
-	%x{npm --version}.chomp
+	%x{npm --version 2>/dev/null}.chomp
     end
 end
 Facter.add("pm2version") do
     setcode do
-	%x{pm2 --version}.chomp
+	%x{pm2 --version 2>/dev/null}.chomp
     end
 end
