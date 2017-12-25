c764c7bceb03a14851c3858494f588716fc2bd51
Samuel Martin Moro <samuel@unetresgrossebite.com>
2017-12-25 21:07:40 +0100

    wip(yubi): centos/rhel support

--- modules/common/manifests/config/udev.pp
+++ modules/common/manifests/config/udev.pp
@@ -1,7 +1,11 @@
 class common::config::udev {
+    if ($operatingsystem == "RedHat" or $operatingsystem == "CentOS") {
+	$reloadcmd = "udevadm control --reload-rules && udevadm trigger"
+    } else { $reloadcmd = "service udev restart" }
+
     exec {
 	"Reload udev configuration":
-	    command     => "service udev restart",
+	    command     => $reloadcmd,
 	    onlyif      => "test -d /etc/udev/rules.d",
 	    path        => "/usr/local/sbin:/usr/sbin:/sbin:/usr/local/bin:/usr/bin:/bin",
 	    refreshonly => true;
--- modules/common/manifests/physical/u2f.pp
+++ modules/common/manifests/physical/u2f.pp
@@ -2,12 +2,12 @@ class common::physical::u2f {
     if ($kernel == "Linux") {
 	file {
 	    "Install Yubiko udev rules":
-		group  => lookup("gid_zero"),
-		mode   => "0644",
-		notify => Exec["Reload udev configuration"],
-		owner  => root,
-		path   => "/etc/udev/rules.d/70-u2f.rules",
-		source => "puppet:///modules/common/yubi-u2f";
+		content => template("common/yubi-u2f.erb"),
+		group   => lookup("gid_zero"),
+		mode    => "0644",
+		notify  => Exec["Reload udev configuration"],
+		owner   => root,
+		path    => "/etc/udev/rules.d/70-u2f.rules";
 	}
     }
 }
