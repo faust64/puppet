10b0a52eeb8cb810b0ef00567cd894a876cfc878
Samuel Martin Moro <samuel@unetresgrossebite.com>
2018-01-06 16:21:48 +0100

    fix(postfix): centos chroot (?!)
    likely I never deployed postfix with these modules on centos, as the
    stackoverflow post that got me out was pretty old

--- modules/postfix/manifests/rhel.pp
+++ modules/postfix/manifests/rhel.pp
@@ -2,4 +2,20 @@ class postfix::rhel {
     common::define::package {
 	"postfix":
     }
+
+    exec {
+	"Prepare postfix chroot setup script":
+	    command     => "cp -p /usr/share/doc/postfix-*/examples/chroot-setup/LINUX2 /usr/src/postfix-chroot-setup && chmod +x /usr/src/postfix-chroot-setup",
+	    creates     => "/usr/src/postfix-chroot-setup",
+	    cwd         => "/",
+	    notify      => Exec["Setup Postfix chroot"],
+	    path        => "/usr/bin:/bin",
+	    require     => Common::Define::Package["postfix"];
+	"Setup Postfix chroot":
+	    command     => "/usr/src/postfix-chroot-setup",
+	    cwd         => "/",
+	    path        => "/usr/sbin:/sbin:/usr/bin:/bin",
+	    refreshonly => true,
+	    require     => Common::Define::Service["postfix"];
+    }
 }
