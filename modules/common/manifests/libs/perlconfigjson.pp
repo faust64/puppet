3addcdae6cae02c9a186d292f1de4b7fef567b45
Samuel Martin Moro <samuel@unetresgrossebite.com>
2018-01-06 17:20:47 +0100

    fix(centos7): perl-Config-JSON's gone?

--- modules/common/manifests/libs/perlconfigjson.pp
+++ modules/common/manifests/libs/perlconfigjson.pp
@@ -5,7 +5,23 @@ class common::libs::perlconfigjson {
 	}
 	"CentOS", "RedHat": {
 	    if ($os['release']['major'] == "7") {
-		$what = "perl-Config-Any"
+		$what = [ "perl-Config-Any", "perl-Any-Moose" ]
+
+		file {
+		    "Create perl-Config-JSON directory":
+			ensure  => directory,
+			group   => lookup("gid_zero"),
+			mode    => "0755",
+			owner   => root,
+			path    => "/usr/lib64/perl5/Config";
+		    "Install perl-Config-JSON":
+			group   => lookup("gid_zero"),
+			mode    => "0644",
+			owner   => root,
+			path    => "/usr/lib64/perl5/Config/JSON.pm",
+			require => Common::Define::Package["perl-Any-Moose"],
+			source  => "puppet:///modules/common/perl-Config-JSON.pm";
+		}
 	    } else {
 		$what = "perl-Config-JSON"
 	    }
