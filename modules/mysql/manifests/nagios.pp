f3fbe7ab7dd192f0de9642a26a80e6f9b63dc312
Samuel Martin Moro <samuel@unetresgrossebite.com>
2019-09-29 08:25:06 +0200

    wip(mysql)

--- modules/mysql/manifests/nagios.pp
+++ modules/mysql/manifests/nagios.pp
@@ -1,12 +1,14 @@
 class mysql::nagios {
+    if ($mspw != "") {
+	$args = [ "-u $msuser", "-p '$mspw'" ]
+    } elsif ($msuser != "") {
+	$args = [ "-u $msuser" ]
+    } else { $args = [] }
+
     nagios::define::probe {
 	"mysql":
 	    description   => "$fqdn mysql server",
-	    pluginargs    =>
-		[
-		    "-u $msuser",
-		    "-p '$mspw'"
-		],
+	    pluginargs    => $args,
 	    servicegroups => "databases",
 	    use           => "critical-service";
     }
