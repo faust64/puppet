ab84b2f9273dde619be21edcdbc5de576bb4415a
Samuel Martin Moro <samuel@unetresgrossebite.com>
2020-02-26 01:10:19 +0100

    wip(misc)

--- modules/pixelserver/manifests/scripts.pp
+++ modules/pixelserver/manifests/scripts.pp
@@ -7,5 +7,12 @@ class pixelserver::scripts {
 	    owner  => root,
 	    path   => "/usr/local/sbin/pixelserver",
 	    source => "puppet:///modules/pixelserver/pixelserver";
+	"Install pixelserver wrapper":
+	    group  => lookup("gid_zero"),
+	    mode   => "0750",
+	    notify => Service["pixelserver"],
+	    owner  => root,
+	    path   => "/usr/local/sbin/pixelserver-wrapper",
+	    source => "puppet:///modules/pixelserver/wrapper";
     }
 }
