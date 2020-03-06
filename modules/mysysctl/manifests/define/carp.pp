c925e0aed87b83a7f264baa4fa05e8c75e167d8d
Samuel Martin Moro <samuel@unetresgrossebite.com>
2020-03-06 15:02:42 +0100

    wip(bsd): disables carp preemption

--- modules/mysysctl/manifests/define/carp.pp
+++ modules/mysysctl/manifests/define/carp.pp
@@ -1,19 +1,28 @@
 class mysysctl::define::carp {
+    if (lookup("carp_do_preempt") == true) {
+	$carp_preempt = 1
+    } else {
+	$carp_preempt = 0
+    }
+
     case $operatingsystem {
 	"FreeBSD", "OpenBSD": {
-	    file_line {
+	    common::define::lined {
 		"Set CARP log verbosity":
 		    line    => "net.inet.carp.log=3",
+		    match   => "^net\.inet\.carp\.log",
 		    notify  => Exec["Apply CARP log verbosity"],
 		    path    => "/etc/sysctl.conf",
 		    require => File["Ensure sysctl.conf present"];
 		"Allow CARP":
 		    line    => "net.inet.carp.allow=1",
+		    match   => "^net\.inet\.carp\.allow",
 		    notify  => Exec["Apply Allow CARP"],
 		    path    => "/etc/sysctl.conf",
 		    require => File["Ensure sysctl.conf present"];
 		"Set CARP Preempt":
-		    line    => "net.inet.carp.preempt=1",
+		    line    => "net.inet.carp.preempt=$carp_preempt",
+		    match   => "^net\.inet\.carp\.preempt",
 		    notify  => Exec["Apply CARP Preempt"],
 		    path    => "/etc/sysctl.conf",
 		    require => File["Ensure sysctl.conf present"];
@@ -31,7 +40,7 @@ class mysysctl::define::carp {
 		    path        => "/sbin",
 		    refreshonly => true;
 		"Apply CARP Preempt":
-		    command     => "sysctl net.inet.carp.preempt=1 sysctl.conf",
+		    command     => "sysctl net.inet.carp.preempt=$carp_preempt",
 		    cwd         => "/",
 		    path        => "/sbin",
 		    refreshonly => true;
