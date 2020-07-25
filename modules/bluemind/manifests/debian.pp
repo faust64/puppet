a7faff28c5966f5b0fcdf1f8fa4832b80c1c9f7c
Samuel MARTIN MORO <samuel@unetresgrossebite.com>
2020-07-25 23:09:33 +0200

    wip(bluemind): plugins

--- modules/bluemind/manifests/debian.pp
+++ modules/bluemind/manifests/debian.pp
@@ -1,6 +1,19 @@
 class bluemind::debian {
     common::define::package {
+	[
+	    "bm-plugin-admin-console-ldap-import",
+	    "bm-plugin-core-ldap-import",
+	    "bm-plugin-core-mapi-support"
+	]:
+	    notify => Exec["Restarts BlueMind"];
 	"python-memcache":
+    }
+
+    exec {
+	"Restarts BlueMind":
+	    command     => "bmctl restart",
+	    path        => "/sbin:/usr/sbin:/bin:/usr/bin",
+	    refreshonly => true;
     }
 
     Common::Define::Package["python-memcache"]
