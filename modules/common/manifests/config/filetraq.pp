a05b854804e8fd397c7954a8ac9caa6c442c1e53
Samuel MARTIN MORO <samuel@unetresgrossebite.com>
2020-09-02 14:24:06 +0200

    fix(filetraq)

--- modules/common/manifests/config/filetraq.pp
+++ modules/common/manifests/config/filetraq.pp
@@ -4,6 +4,16 @@ class common::config::filetraq {
     if ($kernel == "Linux") {
 	$list = [ "/etc/passwd", "/etc/group", "/etc/shadow-", "/etc/shadow",
 		  "/etc/passwd-", "/etc/group-" ]
+
+	file {
+	    "Ensures rc.local exists":
+		content => "",
+		group   => lookup("gid_zero"),
+		mode    => "0755",
+		owner   => root,
+		path    => "/etc/rc.local",
+		replace => "no";
+	}
     } else {
 	$list = [ "/etc/passwd", "/etc/group" ]
     }
